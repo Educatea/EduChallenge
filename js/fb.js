70e6451345b397ffea404422cafabb57c8996422
Juan Gesino <juangesino@gmail.com>
2015-03-15 16:35:08 -0300

    Debug Graph API response

--- js/fb.js
+++ js/fb.js
@@ -6,12 +6,23 @@
   openFB.init({appId: '804318116309818', tokenStore: window.localStorage});
 
 function login() {
+    $.blockUI({ css: {
+        border: 'none',
+        padding: '15px',
+        backgroundColor: '#000',
+        '-webkit-border-radius': '10px',
+        '-moz-border-radius': '10px',
+        opacity: .5,
+        color: '#fff'
+    } });
     openFB.login(
         function(response) {
             if(response.status === 'connected') {
                 localStorage['fb_token'] = response.authResponse.token;
+                done();
                 window.location.href = "main.html";
             } else {
+                done();
                 alert('Facebook login failed: ' + response.error);
             }
         }, {scope: 'email,read_stream,publish_stream'});
@@ -21,7 +32,7 @@ function getInfo() {
     openFB.api({
         path: '/me',
         success: function(data) {
-            console.log(JSON.stringify(data));
+            alert(JSON.stringify(data));
             document.getElementById("userName").innerHTML = data.name;
             document.getElementById("userPic").src = 'http://graph.facebook.com/' + data.id + '/picture?type=small';
             done();
